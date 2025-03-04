2ce8a38d888a29afa0e5f0ac953cf50af99be38b
WaiTgC <waiyanlintg@gmail.com>
2025-03-04 17:51:34 +0700

    updated link connecting

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,10 +7,20 @@ const Contact = () => {
     <div className="contact-page">
       <div className="social-contact">
         <p>Follow Us On</p>
-        <Link className="ig" href="#">
+        <a
+          href="https://www.instagram.com/shalpal_tea_house?igsh=azRydHJ5d2Q2MnZp&fbclid=IwY2xjawIzuk1leHRuA2FlbQIxMAABHfVKIcMUC9Vk71OLHRx8yweWH4qtVEqT4QvyCDxscl-OrvfziCmC8LLVrw_aem_rNz_Fc_7EikM3ZupCIbX1g"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fb"
+        >
           <i className="bi bi-instagram"></i>
-        </Link>
-        <a className="fb" href="#">
+        </a>
+        <a
+          href="https://www.facebook.com/sarkyaml?mibextid=wwXIfr&rdid=te9xxKE2mFMMm7Db"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fb"
+        >
           <i className="bi bi-facebook"></i>
         </a>
       </div>
