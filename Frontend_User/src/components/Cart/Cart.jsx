83b25e548cf6c65571c2ffa1792d2f79d81bcaa4
WaiTgC <waiyanlintg@gmail.com>
2025-04-03 19:44:28 +0700

    Final Code

--- Frontend_User/src/components/Cart/Cart.jsx
+++ Frontend_User/src/components/Cart/Cart.jsx
@@ -3,14 +3,25 @@ import { useLocation } from "react-router-dom";
 import "./Cart.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContextProvider";
+import { useContext } from "react";
 
 const Cart = () => {
   const location = useLocation();
+  const { cart } = useContext(StoreContext);
+
+  // Calculate total number of items in cart
+  const cartItemCount = cart.reduce((total, item) => total + item.count, 0);
 
   return (
     <div className="cart">
       <Link to="/AddCart">
-        <img src={assets.cart} alt="cart" />
+        <div className="cart-container">
+          <img src={assets.cart} alt="cart" />
+          {cartItemCount > 0 && (
+            <span className="cart-count">{cartItemCount}</span>
+          )}
+        </div>
       </Link>
     </div>
   );
